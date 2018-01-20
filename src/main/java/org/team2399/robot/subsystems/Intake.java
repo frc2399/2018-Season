4ae2d80781aa254fcfe5cd5a3c56eef74d7d381d
Anika Rede <anika.rede@gmail.com>
2018-01-20 15:41:17 -0500

    Rough out subsystems
    https://docs.google.com/document/d/1UjOApSqyymIOaE2Wz2ZFamIyGGZT2EuY7bfLmVnDmhE/edit

---
+++ src/main/java/org/team2399/robot/subsystems/Intake.java
@@ -0,0 +1,29 @@
+package org.team2399.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Intake extends Subsystem{
+
+	public Intake() {
+		
+	}
+	
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void setSpeed(double speed) {
+		 
+	 }
+	
+	public void deploy(boolean out) {
+		
+	}
+	
+	public void defaultCommand(Command c) {
+	    	setDefaultCommand(c);
+	    }
+}
