d6877a7ecdbfa4737e25124968940466d4eb350c
ROet <cosmicham20@gmail.com>
2018-01-20 14:23:45 -0500

    Add velocity-based control to DriveTrain

--- src/main/java/org/team2399/robot/subsystems/DriveTrain.java
+++ src/main/java/org/team2399/robot/subsystems/DriveTrain.java
@@ -3,8 +3,11 @@ package org.team2399.robot.subsystems;
 import org.team2399.robot.RobotMap;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -12,20 +15,43 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class DriveTrain extends Subsystem {
-
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
 	
-	TalonSRX leftFrontTalon = new TalonSRX(8);
-	TalonSRX leftMiddleTalon = new TalonSRX(7);
-	TalonSRX leftBackTalon = new TalonSRX(3);
+	private TalonSRX leftFrontTalon = new TalonSRX(8);
+	private TalonSRX leftMiddleTalon = new TalonSRX(7);
+	private TalonSRX leftBackTalon = new TalonSRX(3);
 	
-	TalonSRX rightFrontTalon = new TalonSRX(1);
-	TalonSRX rightMiddleTalon = new TalonSRX(2);
-	TalonSRX rightBackTalon = new TalonSRX(5);
+	private TalonSRX rightFrontTalon = new TalonSRX(1);
+	private TalonSRX rightMiddleTalon = new TalonSRX(2);
+	private TalonSRX rightBackTalon = new TalonSRX(5);
+	
+    public void DriveTrain() {
 
-    public void Drivetrain() {
+    	// timeout constants
+    	leftFrontTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+    	rightFrontTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+    	leftFrontTalon.setSensorPhase(true);
+    	rightFrontTalon.setSensorPhase(true);
     	
+    	// timeout constants
+		leftFrontTalon.configNominalOutputForward(0, 10);
+		leftFrontTalon.configNominalOutputReverse(0, 10);
+		leftFrontTalon.configPeakOutputForward(1, 10);
+		leftFrontTalon.configPeakOutputReverse(-1, 10);
+
+		leftFrontTalon.config_kF(0, 0.5, 10);
+		leftFrontTalon.config_kP(0, 0.5, 10);
+		leftFrontTalon.config_kI(0, 0.0015, 10);
+		leftFrontTalon.config_kD(0, 0.0, 10);
+		
+		rightFrontTalon.configNominalOutputForward(0, 10);
+		rightFrontTalon.configNominalOutputReverse(0, 10);
+		rightFrontTalon.configPeakOutputForward(1, 10);
+		rightFrontTalon.configPeakOutputReverse(-1, 10);
+
+		rightFrontTalon.config_kF(0, 0.5, 10);
+		rightFrontTalon.config_kP(0, 0.5, 10);
+		rightFrontTalon.config_kI(0, 0.0015, 10);
+		rightFrontTalon.config_kD(0, 0.0, 10);
     }
     
     public void defaultCommand(Command c) {
@@ -50,8 +76,22 @@ public class DriveTrain extends Subsystem {
 		follow(rightBackTalon, rightFrontTalon);
 	}
 
+    public void driveVelocity(double leftVelocity, double rightVelocity) {
+		double leftVelocityForward = leftVelocity * 4096 / 600 * RobotMap.Physical.DriveTrain.LEFT_FORWARD ;
+		double rightVelocityForward = rightVelocity * 4096 / 600 * RobotMap.Physical.DriveTrain.RIGHT_FORWARD;
+		
+		leftFrontTalon.set(ControlMode.Velocity, leftVelocityForward);
+		rightFrontTalon.set(ControlMode.Velocity, rightVelocityForward);
+		
+		follow(leftMiddleTalon, leftFrontTalon);
+		follow(leftBackTalon, leftFrontTalon);
+		follow(rightMiddleTalon, rightFrontTalon);
+		follow(rightBackTalon, rightFrontTalon);
+    }
+
 	@Override
 	protected void initDefaultCommand() {
+		
 	}
 }
 
