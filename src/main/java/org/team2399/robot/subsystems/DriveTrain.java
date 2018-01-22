5594a5c4a1acbfaafebfec088ab7a67652c643ea
ROet <cosmicham20@gmail.com>
2018-01-21 19:52:36 -0500

    Modified PID Values and Created Conversion

--- src/main/java/org/team2399/robot/subsystems/DriveTrain.java
+++ src/main/java/org/team2399/robot/subsystems/DriveTrain.java
@@ -20,13 +20,18 @@ public class DriveTrain extends Subsystem {
 	private static final double DRIVETRAIN_KP = 0.3;
 	private static final double DRIVETRAIN_KF = 0.33;
 	
-	private static final double DRIVETRAIN_FAST_KP = 0.15;
+	private static final double DRIVETRAIN_FAST_KP = 0.3;
 	private static final double DRIVETRAIN_FAST_KI = 0.001;
 	private static final double DRIVETRAIN_FAST_KD = 0;
 	private static final double DRIVETRAIN_FAST_KF = 0.13;
 	
 	
 	private static final int CAN_TIMEOUT = 0;
+	
+	private double desiredLeftVelPrev;
+	private double desiredRightVelPrev;
+	private double actualLeftVelPrev;
+	private double actualRightVelPrev;
 	
 	
 	private TalonSRX leftFrontTalon;
@@ -59,20 +64,25 @@ public class DriveTrain extends Subsystem {
 		leftFrontTalon.configPeakOutputForward(1, CAN_TIMEOUT);
 		leftFrontTalon.configPeakOutputReverse(-1, CAN_TIMEOUT);
 
-		leftFrontTalon.config_kF(0, DRIVETRAIN_KF, CAN_TIMEOUT);
-		leftFrontTalon.config_kP(0, DRIVETRAIN_KP, CAN_TIMEOUT);
-		leftFrontTalon.config_kI(0, DRIVETRAIN_KI, CAN_TIMEOUT);
-		leftFrontTalon.config_kD(0, DRIVETRAIN_KD, CAN_TIMEOUT);
+		leftFrontTalon.config_kF(0, DRIVETRAIN_FAST_KF, CAN_TIMEOUT);
+		leftFrontTalon.config_kP(0, DRIVETRAIN_FAST_KP, CAN_TIMEOUT);
+		leftFrontTalon.config_kI(0, DRIVETRAIN_FAST_KI, CAN_TIMEOUT);
+		leftFrontTalon.config_kD(0, DRIVETRAIN_FAST_KD, CAN_TIMEOUT);
 		
 		rightFrontTalon.configNominalOutputForward(0, CAN_TIMEOUT);
 		rightFrontTalon.configNominalOutputReverse(0, CAN_TIMEOUT);
 		rightFrontTalon.configPeakOutputForward(1, CAN_TIMEOUT);
 		rightFrontTalon.configPeakOutputReverse(-1, CAN_TIMEOUT);
 
-		rightFrontTalon.config_kF(0, DRIVETRAIN_KF, CAN_TIMEOUT);
-		rightFrontTalon.config_kP(0, DRIVETRAIN_KP, CAN_TIMEOUT);
-		rightFrontTalon.config_kI(0, DRIVETRAIN_KI, CAN_TIMEOUT);
-		rightFrontTalon.config_kD(0, DRIVETRAIN_KD, CAN_TIMEOUT);
+		rightFrontTalon.config_kF(0, DRIVETRAIN_FAST_KF, CAN_TIMEOUT);
+		rightFrontTalon.config_kP(0, DRIVETRAIN_FAST_KP, CAN_TIMEOUT);
+		rightFrontTalon.config_kI(0, DRIVETRAIN_FAST_KI, CAN_TIMEOUT);
+		rightFrontTalon.config_kD(0, DRIVETRAIN_FAST_KD, CAN_TIMEOUT);
+		
+		desiredLeftVelPrev = 0;
+		desiredRightVelPrev = 0;
+		actualLeftVelPrev = 0;
+		actualRightVelPrev = 0;
     }
     
     public void defaultCommand(Command c) {
@@ -96,24 +106,59 @@ public class DriveTrain extends Subsystem {
 		follow(rightMiddleTalon, rightFrontTalon);
 		follow(rightBackTalon, rightFrontTalon);
 	}
+    
+    public double toInPerSecFromNativeTalon(double talonNative) {
+    	return talonNative * (4 * (Math.PI / 4096)) * (24 / 60) * 10;
+    }
+    
+    public double toNativeTalonFromInPerSec(double inPerSec) {
+    	return inPerSec * (4096.0 / (4.0 * Math.PI)) * (60.0 / 24.0) * (1.0 / 10.0);
+    }
 
     public void driveVelocity(double leftVelocity, double rightVelocity) {
-		double leftVelocityForward = leftVelocity * 4096 / 600 * RobotMap.Physical.DriveTrain.LEFT_FORWARD ;
-		double rightVelocityForward = rightVelocity * 4096 / 600 * RobotMap.Physical.DriveTrain.RIGHT_FORWARD;
+		double desiredLeftVelocityForward = toNativeTalonFromInPerSec(leftVelocity) * RobotMap.Physical.DriveTrain.LEFT_FORWARD ;
+		double desiredRightVelocityForward = toNativeTalonFromInPerSec(rightVelocity) * RobotMap.Physical.DriveTrain.RIGHT_FORWARD;
 		
-		leftFrontTalon.set(ControlMode.Velocity, leftVelocityForward);
-		rightFrontTalon.set(ControlMode.Velocity, rightVelocityForward);
+		leftFrontTalon.set(ControlMode.Velocity, desiredLeftVelocityForward);
+		rightFrontTalon.set(ControlMode.Velocity, desiredRightVelocityForward);
 		
 		follow(leftMiddleTalon, leftFrontTalon);
 		follow(leftBackTalon, leftFrontTalon);
 		follow(rightMiddleTalon, rightFrontTalon);
 		follow(rightBackTalon, rightFrontTalon);
 		
-		double[] left = {leftVelocityForward, leftFrontTalon.getSelectedSensorVelocity(0)};
-		double[] right = {rightVelocityForward, rightFrontTalon.getSelectedSensorVelocity(0)};
+		double actualLeftVelocityForward = leftFrontTalon.getSelectedSensorVelocity(0);
+		double actualRightVelocityForward = rightFrontTalon.getSelectedSensorVelocity(0);
 		
-		SmartDashboard.putNumberArray("left", left);
-		SmartDashboard.putNumberArray("right", right);
+		double[] leftVelocityArr = {desiredLeftVelocityForward, actualLeftVelocityForward};
+		double[] rightVelocityArr = {desiredRightVelocityForward, actualRightVelocityForward};
+		
+		SmartDashboard.putNumberArray("leftVelocity", leftVelocityArr);
+		SmartDashboard.putNumberArray("rightVelocity", rightVelocityArr);
+		
+		
+		double desiredLeftAccel = desiredLeftVelocityForward - desiredLeftVelPrev;
+		double desiredRightAccel = desiredRightVelocityForward - desiredRightVelPrev;
+		
+		double actualLeftAccel = actualLeftVelocityForward - actualLeftVelPrev;
+		double actualRightAccel = actualRightVelocityForward - actualRightVelPrev;
+		
+		
+		double[] leftAccelArr = {desiredLeftAccel, actualLeftAccel};
+		double[] rightAccelArr = {desiredRightAccel, actualRightAccel};
+		
+		SmartDashboard.putNumberArray("leftAccel",  leftAccelArr);
+		SmartDashboard.putNumberArray("rightAccel", rightAccelArr);
+		
+		
+		desiredLeftVelPrev = desiredLeftVelocityForward;
+		desiredRightVelPrev = desiredRightVelocityForward;
+		
+		actualLeftVelPrev = actualLeftVelocityForward;
+		actualRightVelPrev = actualRightVelocityForward;
+		
+		
+		
     }
 
 	@Override
