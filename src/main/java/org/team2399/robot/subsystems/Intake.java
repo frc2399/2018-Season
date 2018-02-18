b3a61c58b025ec82247799c592adc25b6d6669eb
ROet <cosmicham20@gmail.com>
2018-02-17 21:58:07 -0500

    Replaced Intake talons with victors

--- src/main/java/org/team2399/robot/subsystems/Intake.java
+++ src/main/java/org/team2399/robot/subsystems/Intake.java
@@ -3,7 +3,9 @@ package org.team2399.robot.subsystems;
 import org.team2399.robot.RobotMap;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.IMotorController;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Command;
@@ -11,14 +13,14 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Intake extends Subsystem{
 
-	private TalonSRX leftTalon, rightTalon;
+	private IMotorController left, right;
 	private DoubleSolenoid extender, grabber;
 	public boolean isExtended;
 	
 	//factor later
 	public Intake() {
-		leftTalon = new TalonSRX(10);
-	//	rightTalon = new TalonSRX(0);
+		left = new VictorSPX(9);
+		right = new VictorSPX(10);
 		
 		extender = new DoubleSolenoid(RobotMap.CAN.PCM, RobotMap.PCM.EXTENDER_OUT, RobotMap.PCM.EXTENDER_IN);
 		grabber = new DoubleSolenoid(RobotMap.CAN.PCM, RobotMap.PCM.GRABBER_OUT, RobotMap.PCM.GRABBER_IN);
@@ -27,7 +29,7 @@ public class Intake extends Subsystem{
 	
 	// forward is positive, backwards is negative, -1 to 1
 	public void setSpeed(double speed) {
-		leftTalon.set(ControlMode.PercentOutput, speed);
+		left.set(ControlMode.PercentOutput, speed);
 	//	rightTalon.set(ControlMode.PercentOutput, speed);
 	}
 	
